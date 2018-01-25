85bdce232797b281e2b6ca9defdba0e93d9ae31b
Kishoj Bajracharya <kishoj@gmail.com>
2018-01-25 22:54:20 +0700

    Initial commit

---
+++ src/main/java/com/example/cqrs/ActorBasedCQRS/ActorBasedCqrsApplication.java
@@ -0,0 +1,12 @@
+package com.example.cqrs.ActorBasedCQRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorBasedCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorBasedCqrsApplication.class, args);
+	}
+}
